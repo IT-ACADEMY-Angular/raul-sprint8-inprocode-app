0eb7fe03e35a8d00dbb834218c0193b155a9071f
rawl0408 <raulin4bcn@gmail.com>
2024-12-10 13:48:39 +0100

    Sprint 8 Ejercicio 1 - Navbar created + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ChartComponent } from './components/chart/chart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta predeterminada
+    { path: 'home', component: HomeComponent },
+    { path: 'mapa', component: MapComponent },
+    { path: 'full-calendar', component: CalendarComponent },
+    { path: 'graficos', component: ChartComponent },
+    { path: '**', redirectTo: '/home' } // Ruta wildcard
+];
